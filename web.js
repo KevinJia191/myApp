3e4a4101fe4c31f03ddc9529f264c55b6356b4c2
Kevin Jia <kevin_jia191@yahoo.com>
2014-02-13 11:58:27 -0800

    grio

--- web.js
+++ web.js
@@ -55,8 +55,8 @@ function userModel(){
                 return this.ERR_BAD_USER_EXISTS;
             }
             else{
-                console.log("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1;");
-                client.query("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1;");
+                console.log("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
+                client.query("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
                 console.log("just inserted " + user + ", " + password + ", 1 into login_info");
                 return this.SUCCESS;
             }
