3cbd91e8583a2175473c40485bf010399de9f10b
Kevin Jia <kevin_jia191@yahoo.com>
2014-02-13 11:08:11 -0800

    itie

--- web.js
+++ web.js
@@ -102,7 +102,7 @@ app.post('/signup', function(req, res) {
     console.log("user="+username);
     console.log("pass="+password);
 
-    var body = "<button onclick='window.location.assign(\'http://fast-brook-9858.herokuapp.com\')'"+">Click me</button>";
+    var body = "<button onclick='goToHome()'>"+">Click me</button>";
     var model = new userModel();
     var temp = model.add(username, password);
     
@@ -125,3 +125,7 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+function goToHome(){
+    window.location.assign("http://fast-brook-9858.herokuapp.com/");
+}
