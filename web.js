93247e9cbfb1082e9c33811a118b8d9f7b599a40
Kevin Jia <kevin_jia191@yahoo.com>
2014-02-14 11:44:45 -0800

    hi

--- web.js
+++ web.js
@@ -84,7 +84,7 @@ function TestUsers(){
 }
 
 function UsersModel(){
-    
+    var self = this;
     this.ERR_BAD_CREDENTIALS = -1;
     this.ERR_BAD_USER_EXISTS = -2;
     this.ERR_BAD_USERNAME = -3;
@@ -131,7 +131,7 @@ function UsersModel(){
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             if(user == ""){
                 console.log("got a username thats an empty string");
-                var resultingErrCode = UsersModel.ERR_BAD_USERNAME;
+                var resultingErrCode = this.ERR_BAD_USERNAME;
                 callback(resultingErrCode);
                 //return this.ERR_BAD_USERNAME;
             }
@@ -143,14 +143,14 @@ function UsersModel(){
                 //console.log(result);
                 if(result.rows.length > 0){
                     console.log("tried to add already existing user");
-                    var resultingErrCode = UsersModel.ERR_BAD_USER_EXISTS;
+                    var resultingErrCode = this.ERR_BAD_USER_EXISTS;
                     callback(resultingErrCode);
                     //return this.ERR_BAD_USER_EXISTS;
                 }
                 else{
                     console.log("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
                     client.query("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
-                    var resultingErrCode = UsersModel.SUCCESS;
+                    var resultingErrCode = this.SUCCESS;
                     callback(resultingErrCode);
                     //return this.SUCCESS;
                 }
