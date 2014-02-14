8a74d09956fddf768e54e621e956de6764b0a829
Kevin Jia <kevin_jia191@yahoo.com>
2014-02-14 11:38:56 -0800

    hi

--- web.js
+++ web.js
@@ -86,13 +86,13 @@ function TestUsers(){
 
 function UsersModel(){
     
-    this.ERR_BAD_CREDENTIALS = -1;
-    this.ERR_BAD_USER_EXISTS = -2;
-    this.ERR_BAD_USERNAME = -3;
-    this.ERR_BAD_PASSWORD = -4;
-    this.MAX_PASSWORD_LENGTH = 128;
-    this.MAX_USERNAME_LENGTH = 128;
-    this.SUCCESS = 1;
+    var ERR_BAD_CREDENTIALS = -1;
+    var ERR_BAD_USER_EXISTS = -2;
+    var ERR_BAD_USERNAME = -3;
+    var ERR_BAD_PASSWORD = -4;
+    var MAX_PASSWORD_LENGTH = 128;
+    var MAX_USERNAME_LENGTH = 128;
+    var SUCCESS = 1;
     this.login = login;
     this.add = add;
     this.TESTAPI_resetFixture = TESTAPI_resetFixture;
@@ -132,7 +132,7 @@ function UsersModel(){
         pg.connect(process.env.DATABASE_URL, function(err, client, done) {
             if(user == ""){
                 console.log("got a username thats an empty string");
-                var resultingErrCode = this.ERR_BAD_USERNAME;
+                var resultingErrCode = ERR_BAD_USERNAME;
                 callback(resultingErrCode);
                 //return this.ERR_BAD_USERNAME;
             }
@@ -144,14 +144,14 @@ function UsersModel(){
                 //console.log(result);
                 if(result.rows.length > 0){
                     console.log("tried to add already existing user");
-                    var resultingErrCode = this.ERR_BAD_USER_EXISTS
+                    var resultingErrCode = ERR_BAD_USER_EXISTS;
                     callback(resultingErrCode);
                     //return this.ERR_BAD_USER_EXISTS;
                 }
                 else{
                     console.log("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
                     client.query("INSERT INTO login_info (username, password, count) VALUES (\'"+user+"\', \'"+password+"\',1);");
-                    var resultingErrCode = this.SUCCESS;
+                    var resultingErrCode = SUCCESS;
                     callback(resultingErrCode);
                     //return this.SUCCESS;
                 }
