09e1198c585032828be7495e9bb8e57fd809730b
Kevin Jia <kevin_jia191@yahoo.com>
2014-02-13 21:47:43 -0800

    hi

--- web.js
+++ web.js
@@ -2,25 +2,32 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-
-
-var SUCCESS               =   1
-var ERR_BAD_CREDENTIALS   =  -1
-var ERR_USER_EXISTS       =  -2
-var ERR_BAD_USERNAME      =  -3
-var ERR_BAD_PASSWORD      =  -4
-var MAX_USERNAME_LENGTH = 128
-var MAX_PASSWORD_LENGTH = 128
+//var pg = require('pg');
+//var connection = new pg.Client(process.env.DATABASE_URL);
+//connection.connect();
+app.configure(function(){ app.use(express.bodyParser()); app.use(app.router); });
 
 app.use(logfmt.requestLogger());
 
 app.get("/", function(req,res) {
-    res.set('Content-Type': 'text/plain');
-    res.end("Hi ");
+    res.set('Content-Type', 'text/plain');
+    res.send("Hi :D");
+    /*
+    connection.query("create table users ( user TEXT primary key, password TEXT, count integer)", function(err) {
+    console.log(err);
+    connection.query("insert into users(user, password, integer) values ('a', 'b', 1)", function(err) {
+        console.log(err);
+        res.end("FINISHED");
+    }
+            );
+    });
+    */
 });
 
-app.get("/users/login", function(req, res) {
-    res.set('Content-Type': 'text/plain');
+app.post("/users/login", function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    console.log("SWAG BEAST");
+    console.log(req.body);
     res.end("swag");
 });
 
